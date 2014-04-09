20ed61332d594564669f993e86323e3121be0006
Thein Tun Aung <thein@rekahealth.com>
2014-04-09 22:10:24 +0800

    Initial Commit

---
+++ focusonline/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  focusonline
+//
+//  Created by Thein Tun Aung on 9/4/14.
+//  Copyright (c) 2014 7r48. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ focusonlineTests/focusonlineTests.m
@@ -0,0 +1,34 @@
+//
+//  focusonlineTests.m
+//  focusonlineTests
+//
+//  Created by Thein Tun Aung on 9/4/14.
+//  Copyright (c) 2014 7r48. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface focusonlineTests : XCTestCase
+
+@end
+
+@implementation focusonlineTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
